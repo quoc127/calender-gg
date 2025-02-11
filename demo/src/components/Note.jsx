b734f3fd6ec667af1a2e80f453e5010a0e12851a
quoc127 <nquoc160200@gmail.com>
2025-02-11 11:07:56 +0700

    add note

--- demo/src/components/Note.jsx
+++ demo/src/components/Note.jsx
@@ -41,38 +41,25 @@ export const Note = ({ isOpen, setIsOpen }) => {
                     - Backend nên trả về kiểu dữ liệu là JSON bởi vì JSON có cú
                     pháp đơn giản, rõ ràng, dễ hiểu, giúp FE dễ dàng xử lý hơn
                     vì cấu trúc tương tự Object, nhẹ hơn, được hỗ trợ ở nhiều
-                    ngôn ngữ và thư viện
+                    ngôn ngữ và thư viện.
                   </li>
                 </ul>
               </div>
               <div>
-                2. Bởi vì em sử dụng thư viện &quot;rrule&quot; để xử lý việc
-                lặp lại sự kiện nên nên cách xử lý ban đầu của em như sau:
-                <pre className="whitespace-pre-wrap text-sm">
-                  {`const initialEvents = [
-  {
-    id: 1,
-    title: "First Session with Alex Stan",
-    start: setEventDate("2025-02-07", 10, 30),
-    end: setEventDate("2025-02-07", 11, 30),
-    type: "appointment",
-    "repeat": {
-      "type": "weekly",
-      "interval": 1,
-      "end": "2025-06-30T10:00:00"
-    }
-  },
-];`}
-                </pre>
-                Em sẽ kiểm tra xem sự kiện nào có chứa thuộc tính
-                &quot;repeat&quot; thì sẽ xử lý lặp lại sự kiện và kiểm tra xem
-                nó lặp lại theo tuần, tháng, năm, số lần lặp và ngày kết thúc.
+                2. Cách em sử dụng là phương pháp thủ công, tức là nhập các sự
+                kiện lập lại vào từng ngày mong muốn. Sau khi đã thêm sự kiện
+                thì em sẽ gọi API get evnets để lấy danh sách các sự kiện và
+                hiển thị ra giao diện.
               </div>
               <div>
-                4. Dự án giúp em cải thiện kỹ năng từ thiết kế UI đến làm
-                việc cơ sở dữ liệu, cải thiện UX/UI. Sử dụng React Calendar và
-                FullCalendar để hiển thị lịch, tùy chỉnh giao diện theo yêu cầu.
-                <hr className="my-4"/>
+                4. Dự án giúp em cải thiện kỹ năng lập trình FE và BE với dự án
+                Google-Calendar, từ việc thiết kế UI, cơ sở dữ liệu, cải thiện
+                trải nghiệm người dùng. Sử dụng React Calendar và FullCalendar
+                để hiển thị lịch, tùy chỉnh giao diện theo yêu cầu.
+                <hr className="my-4" />
+                5. Nếu làm tiếp dự án thì em sẽ tìm hiểu và thêm tính năng gửi
+                thông báo cho người dùng biết khi nào sự kiện sẽ diễn ra trước
+                3-5 phút.
               </div>
             </div>
           </DrawerDescription>
